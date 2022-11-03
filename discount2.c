93e6d2cdf1a3ba0a75d5b0386efdc68bfb1a030d
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-03 20:49:01 +0200

    .

--- discount2.c
+++ discount2.c
@@ -1,8 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float discount(float n);
+
 int main(void)
 {
     float price = get_float ("What's the item's price? ");
+    float sale = discount(price);
+    printf("%.2f\n", sale);
     
+}
+
+
+float discount(float n)
+{
+    return n * 0.85;
 }
