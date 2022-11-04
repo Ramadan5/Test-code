6246017944b00352fc327dd47dc3a1977e8b44f7
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-04 10:30:39 +0200

    .

---
+++ mario.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("How many question marks? ");
+    for (int i = 0; i < n; i++)
+    printf("?");
+    printf("\n");
+}
