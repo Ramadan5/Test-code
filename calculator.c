3428f69df3b23e4eb0162d3520721f431354dd0d
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-04 13:40:51 +0200

    .

--- calculator.c
+++ calculator.c
@@ -3,23 +3,23 @@
 
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
-    char c = get_char("What's the operation you're looking for? ");
+    float x = get_float ("x: ");
+    float y = get_float ("y: ");
+    char c = get_char ("What's the operation you're looking for? ");
     if (c == '+')
     {
-        printf("%d\n", x + y);
+        printf("%f\n", x + y);
     }
     else if (c == '-')
     {
-        printf("%d\n", x - y);
+        printf("%f\n", x - y);
     }
     else if (c == '*')
     {
-        printf("%d\n", x * y);
+        printf("%f\n", x * y);
     }
     else if (c == '/')
     {
-        printf("%d\n", x / y);
+        printf("%f\n", x / y);
     }
 }
