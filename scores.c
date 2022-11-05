85d7db607828909a7883f9b51110f59c718ebfdd
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-05 18:49:00 +0200

    .

---
+++ scores.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int ("How many scores? ");
+    int scores[n];
+    int i, j;
+
+    for (i = 0; i < n; i++)
+    {
+        scores[i] = get_int ("What's the score? ");
+
+    }
+    
+    for (i = 0; i < n; i++)
+    {
+        j = j + scores[i];
+    }
+
+    printf("%.2f", (float) j / n);
+
+    printf("\n");
+}
