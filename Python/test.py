ab0255fc5c4156238c0caed598540f6e4b74e151
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-11 16:15:11 +0200

    .

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+# print("Hello world!")
+# name = input ("What's your name? ")
+# age = int (input ("How old are you? "))
+
+# if (age < 13):
+#     print ("You're still too young son", age)
+
+# else:
+#     print ("You're welcome to join", name)
+
+import calendar
+
+yy = 2022
+mm = 11
+
+print (calendar.month (yy, mm))
