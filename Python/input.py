238285e7ee110e29a1a7f7cc51d41533ba725f43
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-12 10:18:41 +0200

    .

---
+++ Python/input.py
@@ -0,0 +1,7 @@
+name = input ("What's your name? ")
+age = int (input ("How old are you? "))
+
+if (age < 13):
+    print ("Hello", name, ", You're still too young!")
+else:
+    print ("Hello", name, ", You're welcome to join us!")
