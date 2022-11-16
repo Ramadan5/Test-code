91388e16726f5c4a66ef7c663e438a43726f0568
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-16 10:23:13 +0200

    .

---
+++ Python/dictionary.py
@@ -0,0 +1,14 @@
+# city1 = 'Cairo'
+# city2 = 'Giza'
+# city3 = 'Alex'
+# city4 = 'Mansoura'
+
+cities =  {
+    'C': 'Cairo',
+    'G': 'Giza',
+    'A': 'Alex',
+    'M': 'Mansoura',
+}
+
+
+print(cities['G'])
---
+++ Python/list.py
@@ -0,0 +1,23 @@
+# city1 = 'Cairo'
+# city2 = 'Giza'
+# city3 = 'Alex'
+# city4 = 'Mansoura'
+
+cities =  [
+    'Cairo', 'Giza', 'Alex', 'Mansoura',
+]
+
+
+def which_city(i):
+    
+    if (i == 'C'):
+        return 0
+    if (i == 'G'):
+        return 1
+    if (i == 'A'):
+        return 2
+    if (i == 'M'):
+        return 3
+
+
+print(cities[which_city(i = input ("What's the first letter of your city? "))])
