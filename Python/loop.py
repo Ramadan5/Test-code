d9f6fa57b736852fc62460970e1c13324f3e7eef
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-17 10:32:08 +0200

    .

---
+++ Python/loop.py
@@ -0,0 +1,13 @@
+# city1 = 'Cairo'
+# city2 = 'Giza'
+# city3 = 'Alex'
+# city4 = 'Mansoura'
+
+cities =  [
+    'Cairo', 'Giza', 'Alex', 'Mansoura',
+]
+
+
+for city in cities:
+    
+    print (city)
