0bf4b95555067777beba28df8a2359a898e82e08
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-11-27 16:21:28 +0200

    .

---
+++ Python/list2.py
@@ -0,0 +1,13 @@
+fruit = [
+    'apples',
+    'strawberries',
+    'oranges',
+    'strawberries',
+    'blueberries',
+    'watermelons',
+]
+
+print (fruit.count('strawberries'))
+print (fruit.count('apples'))
+
+print (fruit.pop())
