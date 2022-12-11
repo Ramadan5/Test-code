79a9cbda89b144dc03cf3904fc4f2650d084207e
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-12-11 12:10:38 +0200

    .

--- C/phonebook.c
+++ C/phonebook.c
@@ -6,14 +6,13 @@
 
 int main(void)
 {
-    int i;
     string names[] = {"Muhammad", "Hesham"};
     string numbers[] = {"+20111-2535-915", "+20106-8277-428"};
     string x = get_string("Who are you looking for? ");
     
     x[0] = toupper(x[0]);
 
-    for (i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
         if (strcmp(names[i], x) == 0)
         {
