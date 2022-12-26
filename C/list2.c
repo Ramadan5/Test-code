23fce91af0b250dd09329728389e944e677a651f
Ramadan5 <MuhammadHesham78@Gmail.com>
2022-12-26 23:00:02 +0200

    .

---
+++ C/list2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    int n;
+    printf("How big of an array you need? ");
+    scanf("%i", &n);
+
+    int *list = malloc(n * sizeof(int));
+    if (list == NULL)
+    {
+        printf("Error occured!");
+        return 1;
+    }
+    
+    for (int i = 0, j = 1; i < n; i++, j++)
+    {
+        list[i] = j;
+        printf("%i\n", list[i]);
+    }
+
+    printf("\n");
+    char *q = malloc(1);
+    printf("Would you like to make any changes?(Y/N) ");
+    scanf("%s", q);
+
+    if (strcmp(q, "Y") == 0 || strcmp(q, "y") == 0)
+    {
+        free(q);
+
+        int b;
+        printf("How big of an array you now need? ");
+        scanf("%i", &b);
+
+        int *t = realloc(list, b * sizeof(int));
+        if (t == NULL)
+        {
+            printf("Error occured!");
+            return 1;
+        }
+        
+        for (int i = n - 1, j = n; i < b; i++, j++)
+        {
+            t[i] = j;
+        }
+
+        list = t;
+
+        for (int i = 0; i < b; i++)
+        {
+            printf("%i\n", list[i]);
+        }
+    }
+
+    free(list);
+}
